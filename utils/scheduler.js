f5a896f498f4faa87c34c4f8b0f7f729f87ba77b
Martin Pravda <martin.pravda@gmail.com>
2025-02-10 18:45:08 +0100

    demo scheduler

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -4,10 +4,16 @@
 /*jslint browser */
 
 function make_scheduler(pool_size) {
-    const pool = new Map();
-
+    const pool = new Map(); 
+    let is_paused = false;
+    
     function schedule(callback) {
         if (pool.size > pool_size) {
+            return;
+        }
+
+        if (is_paused === true) {
+            pool.set(callback, undefined);
             return;
         }
 
@@ -19,12 +25,14 @@ function make_scheduler(pool_size) {
     }
 
     function pause() {
+        is_paused = true;
         pool.forEach(function (ignore, key) {
             pool.set(key, undefined);
         });
     }
 
     function resume() {
+        is_paused = false;
         pool.forEach(function (ignore, callback) {
             schedule(callback);
         });
@@ -38,13 +46,38 @@ function make_scheduler(pool_size) {
         pool.clear();
     }
 
+    function size() {
+        return pool.size;
+    }
+
     return Object.freeze({
         clear,
         is_full,
         pause,
         resume,
-        schedule
+        schedule,
+        size
     });
 }
+
+//demo const scheduler = make_scheduler(5);
+
+//demo scheduler.pause();
+
+//demo scheduler.schedule(function (next) {
+//demo     console.log("A scheduled task 1");
+//demo     next();
+//demo });
+//demo scheduler.schedule(function (next) {
+//demo     console.log("A scheduled task 2");
+//demo     next();
+//demo });
+
+//demo scheduler.size() === 2;
+
+//demo scheduler.resume();
+//demo scheduler.size() === 0;
+
+
 
 export default Object.freeze(make_scheduler);
