f178d77b19cba150dca3d3b79c8b8fc54e488563
Martin Pravda <martin.pravda@gmail.com>
2025-02-10 15:30:09 +0100

    demo queue

--- utils/queue.js
+++ utils/queue.js
@@ -3,7 +3,7 @@
 
 // This is a simple wrapper around the array primitive
 
-/*jslint */
+/*jslint browser, node*/
 
 function make_queue() {
     let pool = [];
@@ -37,5 +37,19 @@ function make_queue() {
         size
     });
 }
+
+//demo const queue = make_queue();
+//demo queue.push(1);
+//demo queue.push(2);
+//demo queue.push(3);
+//demo queue.size() === 3;
+
+//demo const one = queue.pop();
+//demo queue.size() === 2;
+
+//demo queue.peek() === 2;
+
+//demo queue.clear();
+//demo queue.size() === 0;
 
 export default Object.freeze(make_queue);
