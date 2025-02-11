6bf5014bbbf7e36c997286e3869cdf0ee2ea6c50
Martin Pravda <martin.pravda@gmail.com>
2025-02-11 13:28:55 +0100

    subscribe

--- utils/async_queue.js
+++ utils/async_queue.js
@@ -5,41 +5,75 @@
 
 import make_pubsub from "./pubsub.js";
 import make_scheduler from "./scheduler.js";
+import make_queue from "./queue.js";
 
-function make_async_queue(callback, concurrency_limit = 1) {
+function make_async_queue(concurrency_limit = 1) {
     const pubsub = make_pubsub();
     const scheduler = make_scheduler(concurrency_limit);
+    
+// as long as the listener has not been attached
+// we have to push to the task into the temporal queue
+// of the make_async_queue instance
+    
+    const queue = make_queue();
 
-    function process_task(task) {
-        scheduler.schedule(function (done) {
-            callback(function (data, error) {
-                if (error) {
-                    pubsub.emit("error");
-                }
+// we need to keep track if the subscriber callback has been registered
 
-                task.callback(data, error);
-
-// done callback is a callback from scheduler signaling to release the callback
-// from the scheduler's map
-
-                done();
-            }, task.data);
-        });
-    }
+    let is_subscribed = false;
 
     function push(callback, data) {
 
 // processing automatically starts with the very first push
 
-        process_task({
+        queue.push({
             callback,
             data
         });
+
+        if (is_subscribed) {
+            pubsub.emit("process_task", queue.pop());
+        }
     }
 
     function dispose() {
         scheduler.clear();
         pubsub.clear();
+    }
+
+    function subscribe(callback) {
+        if (is_subscribed) {
+            return;
+        }
+
+        function process_task(task) {
+            scheduler.schedule(function (done) {
+                callback(function (data, error) {
+                    if (error) {
+                        pubsub.emit("error");
+                    }
+
+                    task.callback(data, error);
+
+// done callback is a callback from scheduler signaling to release the callback
+// from the scheduler's map
+
+                    done();
+                }, task.data);
+            });
+        }
+
+        const usubscribe = pubsub.subscribe(
+            "process_task",
+            process_task
+        );
+
+// empty the queue with the enqueued tasks
+
+        Array.from(new Array(queue.size())).forEach(function () {
+            process_task(queue.pop());
+        });
+
+        return Object.freeze(usubscribe);
     }
 
     function properties() {
@@ -64,36 +98,36 @@ function make_async_queue(callback, concurrency_limit = 1) {
         pause: scheduler.pause,
         properties,
         push,
-        resume: scheduler.resume
+        resume: scheduler.resume,
+        subscribe
     });
 }
 
-//done const async_queue = make_async_queue(function (next, num) {
-//done     setTimeout(function () {
-//done         console.log("Async iteration: ", num);
-//done         next();
-//done     }, 1000);
-//done });
+//demo const async_queue = make_async_queue();
 
-//done async_queue.pause();
+//demo function done() {
+//demo     console.log("done");
+//demo }
 
-//done function done() {
-//done     console.log("done");
-//done }
+//demo async_queue.push(done, 0);
+//demo async_queue.push(done, 1);
+//demo async_queue.push(done, 2);
+//demo async_queue.push(done, 3);
+//demo async_queue.push(done, 4);
 
-//done async_queue.push(done, 0);
-//done async_queue.push(done, 1);
-//done async_queue.push(done, 2);
-//done async_queue.push(done, 3);
-//done async_queue.push(done, 4);
+//demo async_queue.subscribe(function (next, num) {
+//demo     setTimeout(function () {
+//demo         console.log("Async iteration: ", num);
+//demo         next();
+//demo     }, 1000);
+//demo });
 
-//done async_queue.resume();
-//done setTimeout(function () {
-//done     async_queue.pause();
-//done });
+//demo setTimeout(function () {
+//demo     async_queue.pause();
+//demo }, 1000);
 
-//done setTimeout(function () {
-//done     async_queue.resume();
-//done }, 2000);
+//demo setTimeout(function () {
+//demo     async_queue.resume();
+//demo }, 2000);
 
 export default Object.freeze(make_async_queue);
