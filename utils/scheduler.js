3cc9f627238649e09075c8d5b0c397cb785ca8b9
Martin Pravda <martin.pravda@gmail.com>
2025-02-11 12:01:33 +0100

    refactor

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -3,12 +3,15 @@
 
 /*jslint browser */
 
+import make_queue from "./queue.js";
+
 function make_scheduler(pool_size) {
     const pool = new Map();
+    const queue = make_queue();
     let is_paused = false;
 
-    function schedule(callback) {
-        if (pool.size > pool_size) {
+    function process(callback) {
+        if (callback === undefined) {
             return;
         }
 
@@ -20,8 +23,20 @@ function make_scheduler(pool_size) {
         pool.set(callback, setTimeout(function () {
             callback(function () {
                 pool.delete(callback);
+                process(queue.pop());
             });
         }));
+    }
+
+    function schedule(callback) {
+        queue.push(callback);
+
+        if (pool.size > pool_size - 1) {
+ 
+            return;
+        }
+
+        process(queue.pop());
     }
 
     function pause() {
@@ -34,12 +49,16 @@ function make_scheduler(pool_size) {
     function resume() {
         is_paused = false;
         pool.forEach(function (ignore, callback) {
-            schedule(callback);
+            process(callback);
         });
     }
 
     function is_full() {
         return pool.size === pool_size;
+    }
+
+    function is_processing() {
+        return pool.size > 0;
     }
 
     function clear() {
@@ -53,6 +72,7 @@ function make_scheduler(pool_size) {
     return Object.freeze({
         clear,
         is_full,
+        is_processing,
         pause,
         resume,
         schedule,
@@ -63,20 +83,41 @@ function make_scheduler(pool_size) {
 //demo const scheduler = make_scheduler(5);
 
 //demo scheduler.pause();
+//demo scheduler.is_processing() === false;
 
-//demo scheduler.schedule(function (next) {
+//demo scheduler.schedule(function (done) {
 //demo     console.log("A scheduled task 1");
-//demo     next();
+//demo     done();
 //demo });
-//demo scheduler.schedule(function (next) {
+//demo scheduler.schedule(function (done) {
 //demo     console.log("A scheduled task 2");
-//demo     next();
+//demo     done();
+//demo });
+//demo scheduler.schedule(function (done) {
+//demo     console.log("A scheduled task 3");
+//demo     done();
+//demo });
+//demo scheduler.schedule(function (done) {
+//demo     console.log("A scheduled task 4");
+//demo     done();
+//demo });
+//demo scheduler.schedule(function (done) {
+//demo     console.log("A scheduled task 5");
+//demo     done();
+//demo });
+//demo scheduler.schedule(function (done) {
+//demo     console.log("A scheduled task 6");
+//demo     done();
 //demo });
 
-//demo scheduler.size() === 2;
+//demo scheduler.is_processing() === true;
+
+// scheduler size is 5 even though we have queued 6 tasks
+//demo scheduler.size() === 5;
 
 //demo scheduler.resume();
 //demo scheduler.size() === 0;
+//demo scheduler.is_processing() === false;
 
 
 
